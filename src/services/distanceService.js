4614a8450b5d736366c168b0943f1a4d96fdcb15
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-27 03:12:04 +0200

    feat: add distance service

---
+++ src/services/distanceService.js
@@ -0,0 +1,7 @@
+import geolib from 'geolib'
+
+export const is60MetersApart = (point1, point2) => {
+  console.log(geolib.getDistance(point1, point2))
+
+  return geolib.getDistance(point1, point2) >= 60
+}
---
+++ test/distanceServiceTest.js
@@ -0,0 +1,34 @@
+import assert from 'assert'
+import {
+  is60MetersApart
+} from '../src/services/distanceService.js'
+
+describe('Distance Service', () => {
+  it('should not detect that 2 points are separated by 60 meters or more', () => {
+    const point1 = {
+      lat: 41.3945014,
+      lng: 2.1745278
+    }
+
+    const point2 = {
+      lat: 41.3942688,
+      lng: 2.1743964
+    }
+
+    assert(!is60MetersApart(point1, point2))
+  })
+
+  it('should detect that 2 points are separated by 60 meters or more', () => {
+    const point1 = {
+      lat: 41.4034884,
+      lng: 2.1813668
+    }
+
+    const point2 = {
+      lat: 41.389449,
+      lng: 2.1748984
+    }
+
+    assert(is60MetersApart(point1, point2))
+  })
+})
