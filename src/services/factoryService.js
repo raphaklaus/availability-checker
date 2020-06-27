15cbbc703fa18ea901462eea88b5e8fa7bcfbddc
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-27 03:14:45 +0200

    feat: add factory for the services

---
+++ src/services/factoryService.js
@@ -0,0 +1,11 @@
+import * as httpService from './httpService.js'
+import * as mongoService from './mongoService.js'
+import * as availabilityService from './availabilityService.js'
+import * as distanceService from './distanceService.js'
+
+export {
+  httpService,
+  mongoService,
+  availabilityService,
+  distanceService
+}
