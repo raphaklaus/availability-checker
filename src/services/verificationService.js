54906ebd61995c1849af5b8fb90807dbd6bd88c6
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-27 03:13:43 +0200

    feat(wip): add verification service

---
+++ src/services/verificationService.js
@@ -0,0 +1,7 @@
+import { distanceService } from './distanceService.js'
+
+export const wasScooterOnRide = (currentAvailability, latestAvailability) => {
+  // TODO 1: Check if scooter was not present in the last availability check
+  // TODO 2: Try to find the latest info of this specific scooter.
+  // If found, check the distance
+}
