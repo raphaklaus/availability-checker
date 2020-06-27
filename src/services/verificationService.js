3caf18b25986b4ce5d85c5b1023fbfe878a7bbd4
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-27 15:51:48 +0200

    feat: implemented verification service

--- src/services/verificationService.js
+++ src/services/verificationService.js
@@ -1,7 +1,22 @@
-import { distanceService } from './distanceService.js'
+import { distanceService, availabilityService } from './factoryService.js'
 
-export const wasScooterOnRide = (currentAvailability, latestAvailability) => {
-  // TODO 1: Check if scooter was not present in the last availability check
-  // TODO 2: Try to find the latest info of this specific scooter.
-  // If found, check the distance
+export const getRecentRodeScooters = async (currentAvailability) => {
+  currentAvailability
+    .filter(async x => {
+      const latestScooterRecord = await isScooterInHistory(x.id)
+
+      if (latestScooterRecord && distanceService.is60MetersApart({
+        lat: x.lat,
+        lng: x.lng
+      }, {
+        lat: latestScooterRecord.lat,
+        lng: latestScooterRecord.lng
+      })) {
+        return x
+      }
+    })
+}
+
+const isScooterInHistory = async (scooterSystemId) => {
+  return availabilityService.findInHistory(scooterSystemId)
 }
