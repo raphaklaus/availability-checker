3490cbe10ca68e4574c0030efdef6aaf8095a173
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-28 03:52:14 +0200

    fix: factory, remove some old stuff, add new

--- src/services/factoryService.js
+++ src/services/factoryService.js
@@ -1,13 +1,15 @@
 import * as httpService from './httpService.js'
 import * as mongoService from './mongoService.js'
-import * as availabilityService from './availabilityService.js'
 import * as distanceService from './distanceService.js'
 import * as verificationService from './verificationService.js'
+import * as rideService from './rideService.js'
+import * as scooterService from './scooterService.js'
 
 export {
   httpService,
   mongoService,
-  availabilityService,
   distanceService,
-  verificationService
+  verificationService,
+  rideService,
+  scooterService
 }
