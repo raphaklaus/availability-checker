910cd8e944d3a3ee96868d63a6de6a3660181ebc
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-28 03:52:39 +0200

    feat: add scooter service

---
+++ src/services/scooterService.js
@@ -0,0 +1,32 @@
+import scooterModel from '../models/scooterModel.js'
+
+export const isEmpty = async () => {
+  return await scooterModel.countDocuments() === 0
+}
+
+export const list = async () => {
+  return scooterModel.find({})
+}
+
+export const insert = async (scooters) => {
+  const bulkDocuments = scooters
+    .map(x => {
+      const { id, ...rest } = x
+
+      return {
+        systemId: id,
+        ...rest
+      }
+    })
+    .map(x => ({
+      updateOne: {
+        filter: {
+          systemId: x.systemId
+        },
+        update: x,
+        upsert: true
+      }
+    }))
+
+  return scooterModel.bulkWrite(bulkDocuments)
+}
